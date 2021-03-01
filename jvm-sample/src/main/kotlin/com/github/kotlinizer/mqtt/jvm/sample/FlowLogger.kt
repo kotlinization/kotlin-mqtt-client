fff21ba0a5dd2ee1619b408f6e47e7fc46c07b6d
Milan Jovic <milanjovic92@live.com>
2021-03-02 00:02:37 +0100

    Improved sample logging.

--- jvm-sample/src/main/kotlin/com/github/kotlinizer/mqtt/jvm/sample/FlowLogger.kt
+++ jvm-sample/src/main/kotlin/com/github/kotlinizer/mqtt/jvm/sample/FlowLogger.kt
@@ -3,6 +3,10 @@ package com.github.kotlinizer.mqtt.jvm.sample
 import com.github.kotlinizer.mqtt.Logger
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import java.time.Instant
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 
 class FlowLogger private constructor(
@@ -11,17 +15,25 @@ class FlowLogger private constructor(
 
     constructor() : this(MutableStateFlow(mutableListOf()))
 
+    private val timeFormatter by lazy {
+        DateTimeFormatter.ofPattern("HH:mm:ss.SSS")
+            .withLocale(Locale.getDefault())
+            .withZone(ZoneId.systemDefault())
+    }
+    private val currentTime: String
+        get() = timeFormatter.format(Instant.now())
+
     override fun logError(message: String, throwable: Throwable?) {
-        logs.value += "${System.currentTimeMillis()} [E] $message " +
-                "${throwable?.message} ${throwable?.stackTraceToString()}"
+        logs.value += "$currentTime [E] $message " +
+                "${throwable?.stackTraceToString()?.replace("\t", "          ")}"
 
     }
 
     override fun logDebug(message: String) {
-        logs.value += "${System.currentTimeMillis()} [D] $message"
+        logs.value += "$currentTime [D] $message"
     }
 
     override fun logTrace(message: String) {
-        logs.value += "${System.currentTimeMillis()} [T] $message"
+        logs.value += "$currentTime [T] $message"
     }
 }
--- jvm-sample/src/main/kotlin/com/github/kotlinizer/mqtt/jvm/sample/Main.kt
+++ jvm-sample/src/main/kotlin/com/github/kotlinizer/mqtt/jvm/sample/Main.kt
@@ -1,9 +1,10 @@
 package com.github.kotlinizer.mqtt.jvm.sample
 
 import androidx.compose.desktop.Window
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -13,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.github.kotlinizer.mqtt.MqttConnectionConfig
+import com.github.kotlinizer.mqtt.MqttConnectionStatus
 import com.github.kotlinizer.mqtt.client.MqttClient
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
@@ -23,23 +25,45 @@ private val mqttClient = MqttClient(flowLogger)
 fun main() = Window(title = "MQTT Client Sample") {
     val scope = rememberCoroutineScope()
     val connectionState = mqttClient.connectionStatusStateFlow.collectAsState()
-    val logs = flowLogger.map { it.joinToString(System.lineSeparator()) }.collectAsState("")
+    val logs = flowLogger.map { it.reversed() }.collectAsState(emptyList())
     MaterialTheme {
         Column(Modifier.fillMaxSize(), Arrangement.spacedBy(5.dp)) {
-            Button(
-                modifier = Modifier.align(Alignment.CenterHorizontally),
-                onClick = {
-                    scope.launch {
-                        mqttClient.connect(
-                            MqttConnectionConfig(serverUri = "tcp://localhost:1883")
-                        )
-                    }
-                }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
             ) {
-                Text("CONNECT")
+                Text(connectionState.value.name)
+                Button(
+                    onClick = {
+                        scope.launch {
+                            mqttClient.connect(
+                                MqttConnectionConfig(serverUri = "tcp://localhost:1883")
+                            )
+                        }
+                    },
+                    enabled = connectionState.value != MqttConnectionStatus.CONNECTED
+                ) {
+                    Text("CONNECT")
+                }
+                Button(
+                    onClick = {
+                        scope.launch {
+                            mqttClient.disconnect()
+                        }
+                    },
+                    enabled = connectionState.value == MqttConnectionStatus.CONNECTED
+                ) {
+                    Text("DISCONNECT")
+                }
             }
-            Text(connectionState.value.name)
-            Text(logs.value)
+            LazyColumn(
+                reverseLayout = false,
+                state = LazyListState()
+            ) {
+                items(logs.value) { log -> Text(log) }
+            }
         }
     }
 }
+
